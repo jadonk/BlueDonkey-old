5c5182e71a512d5f5381a4834029098cef37ce2e
Demo User <debian@beaglebone.localdomain>
2018-06-05 04:33:17 +0000

    Add simple servo demo

---
+++ servo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import os, sys
+if not os.geteuid() == 0:
+    sys.exit("\nPlease run as root.\n")
+if sys.version_info < (3,0):
+    sys.exit("\nPlease run under python3.\n")
+
+import rcpy, time
+from rcpy.servo import servo1
+rcpy.servo.enable()
+servo1.set(0)
+servo1clk = rcpy.clock.Clock(servo1, 0.02)
+servo1clk.start()
+
+time.sleep(1)
+servo1.set(0.5)
+time.sleep(1)
+servo1.set(-0.5)
+time.sleep(1)
+servo1.set(0)
+time.sleep(1)
+
+servo1clk.stop()
+rcpy.servo.disable()
+
