88d6ecdf26ffebeafea217e859344503912b5f29
Demo User <debian@beaglebone.localdomain>
2018-06-07 20:14:43 +0000

    hacks

--- camera.py
+++ camera.py
@@ -24,8 +24,17 @@ while(capture.isOpened()):
         upper_yellow = numpy.array([190,255,255])
         mask = cv2.inRange(frame, lower_yellow, upper_yellow)
         res = cv2.bitwise_and(frame, frame, mask=mask)
+        #cv2.imwrite('/run/bluedonkey/filtered.jpg', res)
+        gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+        ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+        im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        cnt = contours[0]
+        [vx,vy,x,y] = cv2.fitLine(cnt, cv2.DIST_L2, 0, 0.01, 0.01)
+        lefty = int((-x*vy/vx) + y)
+        righty = int(((160-x)*vy/vx)+y)
+        res = cv2.line(res, (159,righty), (0,lefty), (0,0,255), 2)
         cv2.imwrite('/run/bluedonkey/filtered.jpg', res)
-        
+    
     time.sleep(0.2)
 
 capture.release()
