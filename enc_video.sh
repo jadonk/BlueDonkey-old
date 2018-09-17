249b89a661a0b87cd8a5c0b59a33a85750b514fe
Demo User <debian@bluedonkey-c5cb.localdomain>
2018-09-17 15:42:36 +0000

    Increase frame rate

--- enc_video.sh
+++ enc_video.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 name=bluedonkey-run-`date +%Y%m%d%H%M%S`
 cd /var/lib/cloud9/mnt
-ffmpeg -r 2 -i cam%05d.png ${name}-cam.mp4
-ffmpeg -r 2 -i res%05d.png ${name}-res.mp4
-ffmpeg -r 2 -filter_complex hstack -i ${name}-cam.mp4 -i ${name}-res.mp4 ${name}.mp4
+ffmpeg -r 5 -i cam%05d.png ${name}-cam.mp4
+ffmpeg -r 5 -i res%05d.png ${name}-res.mp4
+ffmpeg -filter_complex hstack -i ${name}-cam.mp4 -i ${name}-res.mp4 ${name}.mp4
 mkdir ${name}
 mv *.png ${name}/
