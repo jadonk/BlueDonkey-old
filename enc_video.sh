f0046d0bc0983c769edfdf1bbf81213728bd3b2f
Demo User <debian@bluedonkey-c5cb.localdomain>
2018-09-17 15:02:07 +0000

    Make separate videos first

--- enc_video.sh
+++ enc_video.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 name=bluedonkey-run-`date +%Y%m%d%S`
 cd /var/lib/cloud9/mnt
-ffmpeg -r 2 -filter_complex hstack -i cam%05d.png -i res%05d.png ${name}.mp4
+ffmpeg -r 2 -i cam%05d.png ${name}-cam.mp4
+ffmpeg -r 2 -i res%05d.png ${name}-res.mp4
+ffmpeg -r 2 -filter_complex hstack -i ${name}-cam.mp4 -i ${name}-res.mp4 ${name}.mp4
 mkdir ${name}
 mv *.png ${name}/
