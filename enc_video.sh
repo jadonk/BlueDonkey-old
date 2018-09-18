be021e94f9e409131a9d4ca7b10ef0c17c6d06e1
Demo User <debian@bluedonkey-c5cb.localdomain>
2018-09-18 17:36:38 +0000

    Upload video of latest

--- enc_video.sh
+++ enc_video.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-name=bluedonkey-run-`date +%Y%m%d%H%M%S`
-#cd /var/lib/cloud9/mnt
-cd /run/bluedonkey
+if [ -z "$1" ]; then
+    name=bluedonkey-run-`date +%Y%m%d%H%M%S`
+else
+    name=$1
+fi
+mkdir -p /var/lib/cloud9/mnt/${name}
+mv /run/bluedonkey/*.png /var/lib/cloud9/mnt/${name}/
+mv /var/lib/cloud9/mnt/*.png /var/lib/cloud9/mnt/${name}/
+cd /var/lib/cloud9/mnt/${name}
 ffmpeg -r 5 -i res%05d.png ${name}.mp4
 #ffmpeg -r 5 -i cam%05d.png ${name}-cam.mp4
 #ffmpeg -r 5 -i res%05d.png ${name}-res.mp4
 #ffmpeg -filter_complex hstack -i ${name}-cam.mp4 -i ${name}-res.mp4 ${name}.mp4
 cp ${name}.mp4 /run/bluedonkey/bluedonkey-run-latest.mp4
-
-mkdir -p /var/lib/cloud9/mnt/${name}
-mv *.png /var/lib/cloud9/mnt/${name}/
-mv ${name}.mp4 /var/lib/cloud9/mnt/
