b785cd5fe60290e33d98774fbbec305e9aaa2121
Jason Kridner <github@hangerhead.com>
2018-10-26 20:22:51 -0400

    Update bluedonkey_run.sh

--- bluedonkey_run.sh
+++ bluedonkey_run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #/usr/bin/mjpg_streamer -i "/usr/lib/mjpg-streamer/input_file.so -e -f ${PWD}" -o "/usr/lib/mjpg-streamer/output_http.so -p 8090 -w /usr/share/mjpg-streamer/www"
 mkdir -p /run/bluedonkey
-chown debian.debian /run/bluedonkey
+#chown debian.debian /run/bluedonkey
 chmod ugo+rwx /run/bluedonkey
 #cp images.html /run/bluedonkey/
 echo 69 > /sys/class/gpio/export
